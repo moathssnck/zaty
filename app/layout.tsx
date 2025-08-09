b7a4f981b605c85cf68e228fa763682311b25184
moathssnck <myneww00@gmail.com>
2025-08-09 15:18:55 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Quick App',
   description: 'Quick with App',
+    viewport: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0",
 }
 
 export default function RootLayout({
