eaf63d233724fa7b5579bd353d5261005334804e
ssdf <myneww00@gmail.com>
2025-08-09 11:54:12 +0000

    ssss

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,13 +4,14 @@ import { getDatabase } from "firebase/database";
 import { doc, getFirestore, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyB6fMi5ZQZGLG-AWW2l59eexC__TTl1VaQ",
+  authDomain: "zaimns.firebaseapp.com",
+  databaseURL: "https://zaimns-default-rtdb.firebaseio.com",
+  projectId: "zaimns",
+  storageBucket: "zaimns.firebasestorage.app",
+  messagingSenderId: "573438865337",
+  appId: "1:573438865337:web:4302dc325aff0e63fd9068",
+  measurementId: "G-4R9XW48TV6",
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
